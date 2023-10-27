413ccf6bb3e0d24908d0f4bd9868a82b21da610b
tajbaba999 <tajbaba153@gmail.com>
2023-10-27 20:31:25 +0530

    Chart in react

--- src/components/Coindetail.jsx
+++ src/components/Coindetail.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Container, Box } from '@chakra-ui/react'
+import React,  { useState } from 'react'
+import Loader from './Loader';
 
 const Coindetail = () => {
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
+
   return (
-    <div>Coindetail</div>
+   <Container maxW={"container.xl"}>
+    {
+      loading ? <Loader/> : (
+        <>
+
+
+        <Box width={"full"} borderWidth={1}>
+          asdf</Box>
+        </>
+      )
+    }
+   </Container>
   )
 }
 
